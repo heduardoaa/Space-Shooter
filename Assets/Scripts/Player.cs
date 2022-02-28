161796508fc88b3ea97bafc2d40e04128c7d227c
Héctor E. Altamirano Aguilar <altamiranohe@globalhitss.com>
2022-02-28 16:57:03 -0600

    calculate movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // public or private reference
+    // data type (int, float, bool, string)
+    // every variable has a name
+    // optional value assigned
+    // public float speed = 3.5f;
+
+    [SerializeField]
+    private float _speed = 3.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position(0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    // class to calculate movement of player
+    void CalculateMovement() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        // new Vector(1,0,0) * -1 - 3.5f - real time
+        // transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        // is the same but only in one line
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // if player position on the y is greater than 0
+        // y position = 0
+        // if (transform.position.y >= 0) {
+        //     transform.position = new Vector3(transform.position.x, 0, 0);
+        // } else if (transform.position.y <= -3.8f) {
+        //     transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        // }
+        // only a code line
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+
+        // if player on the x > 11
+        // x pos = -11
+        // else if player on the x  is less than -11
+        // x pos = 11
+
+        
+        if (transform.position.x > 11.3f) {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        } else if (transform.position.x < -11.3f) {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+}
