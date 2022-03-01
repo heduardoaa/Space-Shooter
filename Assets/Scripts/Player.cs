b9fe9ef3865c5431329f7e82f4737e8dfb296899
Héctor E. Altamirano Aguilar <altamiranohe@globalhitss.com>
2022-03-01 13:08:14 -0600

    Automated Repo Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,7 +33,7 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             Debug.Log("Space Key Pressed");
-            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
         }
     }
 
