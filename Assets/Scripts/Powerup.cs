00fbbc2f9fcbddd6a5a52c2f606bcc8e0fdf9c99
Héctor E. Altamirano Aguilar <altamiranohe@globalhitss.com>
2022-03-07 17:28:50 -0600

    Automated Repo Update

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -9,34 +9,38 @@ public class Powerup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         // mode down at a speed of 3
-         // when we leave the screen, destrpy this object
-         transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        // mode down at a speed of 3
+        // when we leave the screen, destrpy this object
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
-         if ( transform.position.y < -4.5f) {
-             Destroy(this.gameObject);
-         }
+        if (transform.position.y < -4.5f)
+        {
+            Destroy(this.gameObject);
+        }
     }
     // on trigercollition
     // Only be collectable by the player (Hint: use tags)
     // on collected, destroy
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag =="Player") {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
             // communicate with the player script
             // handle to the component i want
             // assing the handle to the component
             Player player = other.transform.GetComponent<Player>();
-            if (player != null) {
+            if (player != null)
+            {
                 player.TripleShotActive();
             }
             Destroy(this.gameObject);
         }
-        
+
     }
 }
