2ad1767eef35453f54b26e093a1d237fd80744fe
Héctor E. Altamirano Aguilar <altamiranohe@globalhitss.com>
2022-03-12 14:43:46 -0600

    Automated Repo Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -79,7 +79,7 @@ public class Player : MonoBehaviour
 
                 }
                 else
-                {
+                { 
                     transform.Translate(direction * (_speed * _speedMultiplier) * Time.deltaTime);
 
                 } */
